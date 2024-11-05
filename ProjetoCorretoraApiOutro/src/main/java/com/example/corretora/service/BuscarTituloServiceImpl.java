bb51a42d2e4029fcbfa4438b92e1200d98ed06c6
Jean <Jean@Jean-PC>
2024-11-05 15:26:17 -0300

    Ajustes no codigo

--- ProjetoCorretoraApiOutro/src/main/java/com/example/corretora/service/BuscarTituloServiceImpl.java
+++ ProjetoCorretoraApiOutro/src/main/java/com/example/corretora/service/BuscarTituloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.corretora.service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,14 @@ import com.example.corretora.datasource.model.Titulo;
 @Service
 public class BuscarTituloServiceImpl {
 	
+	/*private static final Logger LOG = Logger.getLogger(BuscarTituloServiceImpl.class);*/
+	private static final Logger LOG = Logger.getLogger(BuscarTituloServiceImpl.class);
+	
 	@Autowired
 	private TituloRepository tituloRepository;
 	
 	public List<Titulo> buscarTodosOsTitulos(){
+		LOG.info(" Iniciando o serviço ");
 		List<Titulo> listTitulo =  tituloRepository.findAll();
 		return listTitulo;
 		
