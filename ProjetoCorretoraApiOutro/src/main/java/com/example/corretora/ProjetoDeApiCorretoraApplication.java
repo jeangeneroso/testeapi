411c2b16b5ee0b8e6f7a15c0eaa8eef1b130e61f
Jean <Jean@Jean-PC>
2024-11-06 19:06:47 -0300

    Ajustes no POM e na classe:ProjetoDeApiCorretoraApplication

--- ProjetoCorretoraApiOutro/src/main/java/com/example/corretora/ProjetoDeApiCorretoraApplication.java
+++ ProjetoCorretoraApiOutro/src/main/java/com/example/corretora/ProjetoDeApiCorretoraApplication.java
@@ -3,7 +3,7 @@ package com.example.corretora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.corretora"})
 public class ProjetoDeApiCorretoraApplication {
 
 	public static void main(String[] args) {
